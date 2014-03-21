cf0b229ac44b2dba88513955b5a59a712f529333
Ronny <aRTieitcH@hotmail.de>
2014-03-21 11:04:12 +0100

    Schlüssel zum starten eingefügt

--- Android/Fernsteuerung/gen/de/htw_berlin/Fernsteuerung/R.java
+++ Android/Fernsteuerung/gen/de/htw_berlin/Fernsteuerung/R.java
@@ -22,27 +22,30 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int gear=0x7f020001;
-        public static final int gear1=0x7f020002;
-        public static final int gearknueppel=0x7f020003;
-        public static final int gearoff=0x7f020004;
-        public static final int gearon=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int schalter=0x7f020007;
-        public static final int schalteron=0x7f020008;
-        public static final int selectorswitch=0x7f020009;
-        public static final int throttle=0x7f02000a;
-        public static final int throttlebutton=0x7f02000b;
-        public static final int throttlenp=0x7f02000c;
-        public static final int tranzparenz=0x7f02000d;
+        public static final int engineoff=0x7f020001;
+        public static final int engineon=0x7f020002;
+        public static final int gear=0x7f020003;
+        public static final int gear1=0x7f020004;
+        public static final int gearknueppel=0x7f020005;
+        public static final int gearoff=0x7f020006;
+        public static final int gearon=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int schalter=0x7f020009;
+        public static final int schalteron=0x7f02000a;
+        public static final int selectorengine=0x7f02000b;
+        public static final int selectorswitch=0x7f02000c;
+        public static final int throttle=0x7f02000d;
+        public static final int throttlebutton=0x7f02000e;
+        public static final int throttlenp=0x7f02000f;
+        public static final int tranzparenz=0x7f020010;
     }
     public static final class id {
         public static final int AbsoluteLayout1=0x7f09000b;
-        public static final int RelativeLayout1=0x7f090013;
+        public static final int RelativeLayout1=0x7f090014;
         public static final int ServerResponse=0x7f090005;
         public static final int Space1=0x7f090001;
         public static final int TableLayout1=0x7f090000;
-        public static final int action_settings=0x7f09001c;
+        public static final int action_settings=0x7f09001d;
         public static final int btnBreak=0x7f090002;
         public static final int btnSpeed=0x7f09000d;
         public static final int buttonChange=0x7f09000a;
@@ -52,19 +55,20 @@ public final class R {
         public static final int seekBarGravity=0x7f09000c;
         public static final int seekBarSpeed=0x7f09000e;
         public static final int switchGear=0x7f090003;
-        public static final int textView1=0x7f090014;
+        public static final int textView1=0x7f090015;
         public static final int textView2=0x7f090004;
-        public static final int textView3=0x7f090015;
-        public static final int textView4=0x7f090017;
-        public static final int textView5=0x7f090016;
-        public static final int textView6=0x7f090018;
-        public static final int textView7=0x7f090019;
-        public static final int textView8=0x7f09001a;
-        public static final int textView9=0x7f09001b;
+        public static final int textView3=0x7f090016;
+        public static final int textView4=0x7f090018;
+        public static final int textView5=0x7f090017;
+        public static final int textView6=0x7f090019;
+        public static final int textView7=0x7f09001a;
+        public static final int textView8=0x7f09001b;
+        public static final int textView9=0x7f09001c;
         public static final int textViewDrive=0x7f090011;
         public static final int textViewReverse=0x7f090010;
         public static final int textViewServerOutput=0x7f090006;
         public static final int toggleButtonLight=0x7f09000f;
+        public static final int toggleButtonStartEngine=0x7f090013;
         public static final int webViewCamera=0x7f090012;
     }
     public static final class layout {
